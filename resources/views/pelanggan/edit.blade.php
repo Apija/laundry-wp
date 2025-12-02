43acc745fe1cb68c23c9e3c064aea6c282bb2b0b
Apija <avizasalsa04@gmail.com>
2025-12-02 22:25:43 +0700

    edit

--- resources/views/pelanggan/edit.blade.php
+++ resources/views/pelanggan/edit.blade.php
@@ -1,44 +1,56 @@
 @extends('layout.main')
 @section('content')
-    <div class="container-fluid py-4">
-        <div class="row">
-            <div class="col-12">
-                <div class="card mb-4">
-                    <div class="card-header pb-0">
-                        <h6>Edit Members</h6>
-                    </div>
-                    <div class="card-body px-0 pt-0 pb-2">
-                        <div class="table-responsive p-0">
-                            <form action="{{ route('pelanggan.update', $id->id_pelanggan) }}" method="POST"
-                                enctype="multipart/form-data">
+    <!-- Content wrapper -->
+    <div class="content-wrapper">
+        <!-- Content -->
+        <div class="container-xxl flex-grow-1 container-p-y">
+            <!-- Basic Layout & Basic with Icons -->
+            <div class="row mb-6 gy-6">
+                <!-- Basic Layout -->
+                <div class="col-xxl">
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center justify-content-between">
+                            <h5 class="mb-0">Edit Member</h5>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('pelanggan.update', $id->id_pelanggan) }}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 @method('PUT')
-                                <div class="form-group" style="margin: 0px 100px 0px 100px">
-                                    <label for="nama">Nama :</label>
-                                    <input type="text" class="form-control @error('nama') is-invalid @enderror"
-                                        id="nama" name="nama" value="{{ $id->nama }}">
-                                    @error('nama')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
+                                <div class="row mb-6">
+                                    <label class="col-sm-2 col-form-label" for="nama">Nama</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control @error('nama') is-invalid @enderror"
+                                            id="nama" name="nama" value="{{ $id->nama }}">
+                                        @error('nama')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form-group" style="margin: 0px 100px 0px 100px">
-                                    <label for="no_hp">Nomor Telepon :</label>
-                                    <input type="text" class="form-control @error('no_hp') is-invalid @enderror"
-                                        id="no_hp" name="no_hp" value="{{ $id->no_hp }}">
-                                    @error('no_hp')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
+                                <div class="row mb-6">
+                                    <label class="col-sm-2 col-form-label" for="no_hp">Nomor Telepon :</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control @error('no_hp') is-invalid @enderror"
+                                            id="no_hp" name="no_hp" value="{{ $id->no_hp }}">
+                                        @error('no_hp')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="form-group" style="margin: 0px 100px 0px 100px">
-                                    <label for="alamat">Alamat :</label>
-                                    <textarea class="form-control" id="alamat" name="alamat">{{ $id->alamat }}</textarea>
+                                <div class="row mb-6">
+                                    <label class="col-sm-2 col-form-label" for="alamat">Alamat :</label>
+                                    <div class="col-sm-10">
+                                        <div class="input-group input-group-merge">
+                                            <textarea class="form-control" id="alamat" name="alamat">{{ $id->alamat }}</textarea>
+                                        </div>
+                                    </div>
                                 </div>
-                                <button type="submit" class="btn btn-lg btn-primary btn-lg  mt-4"
-                                    style="margin: 50px 100px 25px 100px">Submit</button>
+                                <div class="row justify-content-end">
+                                    <div class="col-sm-10">
+                                        <button type="submit" class="btn btn-primary">Send</button>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    @endsection
+            @endsection
