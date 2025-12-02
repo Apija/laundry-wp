ea0970c1f4c3a08eff885053cac458dc06c32e9a
Apija <avizasalsa04@gmail.com>
2025-12-02 17:08:29 +0700

    layanan

--- resources/views/layanan/layanan.blade.php
+++ resources/views/layanan/layanan.blade.php
@@ -1,98 +1,55 @@
 @extends('layout.main')
-@section('content')
-    <!-- Navbar -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
-        data-scroll="false">
-        <div class="container-fluid py-1 px-3">
-            <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-                <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                    <div class="input-group">
-                        <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-                        <input type="text" class="form-control" placeholder="Type here...">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <div class="container-fluid py-4">
-        <div class="row">
-            <div class="col-12">
-                <div class="card mb-4">
-                    <div class="card-header pb-0">
-                        <h6>Servis table</h6>
-                        <a href="{{ route('layanan.create') }}" class="btn" style="margin-left: 850px">Add Data</a>
-                    </div>
-                    <div class="card-body px-0 pt-0 pb-2">
-                        <div class="table-responsive p-0">
-                            <table class="table align-items-center mb-0">
-                                <thead>
-                                    <tr>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No
-                                        </th>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama
-                                            Kode Layanan</th>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama
-                                            Layanan</th>
-                                        <th
-                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                            Harga /Kg</th>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                            Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($layanan as $l)
-                                        <tr>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0" style="margin-left: 20px">
-                                                    {{ $loop->iteration }}</p>
-                                            </td>
-                                            <td>
-                                                <div class="d-flex px-2 py-1">
-                                                    <div class="d-flex flex-column justify-content-center">
-                                                        <h6 class="mb-0 text-sm">{{ $l->kode }}</h6>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div class="d-flex px-2 py-1">
-                                                    <div class="d-flex flex-column justify-content-center">
-                                                        <h6 class="mb-0 text-sm">{{ $l->nama_layanan }}</h6>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <p class="text-xs font-weight-bold mb-0">{{ $l->harga_perkilo }}</p>
-                                            </td>
-                                            <td class="align-middle">
-                                                <a href="{{ route('layanan.edit', $l->id_layanan) }}"
-                                                    class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
-                                                    data-original-title="Edit user">
-                                                    Edit
-                                                </a>
-                                                <form id="delete-form-{{ $l->id_layanan }}"
-                                                    action="{{ route('layanan.delete', $l->id_layanan) }}" method="POST"
-                                                    style="display: none;">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                </form>
 
-                                                <a href="#" class="text-secondary font-weight-bold text-xs"
-                                                    data-toggle="tooltip" data-original-title="Delete user"
-                                                    style="margin-left: 50px"
-                                                    onclick="event.preventDefault(); 
-                            if (confirm('Yakin ingin menghapus data ini?')) {
-                                document.getElementById('delete-form-{{ $l->id_layanan }}').submit();
-                            }">
-                                                    Delete
-                                                </a>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+@section('content')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="content-wrapper">
+            <!-- Content -->
+            <div class="container-xxl flex-grow-1 container-p-y">
+                <!-- Basic Bootstrap Table -->
+                <div class="card">
+                    <h5 class="card-header">Layanan</h5>
+                    <div class="table-responsive text-nowrap">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Kode Layanan</th>
+                                    <th>Nama Layanan</th>
+                                    <th>Harga /kg</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-border-bottom-0">
+                                @foreach ($layanan as $l)
+                                    <tr>
+                                        <td>
+                                            <i class="text-danger me-4"></i>
+                                            <span>{{ $loop->iteration }}</span>
+                                        </td>
+                                        <td>{{ $l->kode }}</td>
+                                        <td>{{ $l->nama_layanan }}</td>
+                                        <td>{{ $l->harga_perkilo }}</td>
+                                        <td>
+                                            <div class="dropdown">
+                                                <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                                    data-bs-toggle="dropdown">
+                                                    <i class="icon-base bx bx-dots-vertical-rounded"></i>
+                                                </button>
+                                                <div class="dropdown-menu">
+                                                    <a class="dropdown-item" href="{{ route('layanan.edit', $l->id_layanan) }}""><i
+                                                            class="icon-base bx bx-edit-alt me-1"></i> Edit</a>
+                                                    <a class="dropdown-item" href="javascript:void(0);"><i
+                                                            class="icon-base bx bx-trash me-1"></i> Delete</a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                @endforeach
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
+
+                {{-- Tambahkan bagian dashboard lainnya --}}
             </div>
-        </div>
-    @endsection
+        @endsection
